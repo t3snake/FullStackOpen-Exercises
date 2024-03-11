3d01bd7ccfd9449517a9b9d04386b165f5d7020f
Tarun Menon <t3snake@gmail.com>
2024-03-11 12:24:02 +0530

    Finish ex1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Header = ({name}) => {
+  return (
+    <h1>
+      {name}
+    </h1>
+  )
+}
+
+const Button = ({onClick, name}) => {
+  return (
+    <button onClick={onClick}>
+      {name}
+    </button>
+  )
+}
+
+const Stats = ({name, value}) => {
+  return (
+    <div>
+      {name} {value}
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => {
+    setGood(good + 1)
+  }
+
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const incrementBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header name={"Give Feedback"} />
+      <Button onClick={incrementGood} name={"Good"} />
+      <Button onClick={incrementNeutral} name={"Neutral"} />
+      <Button onClick={incrementBad} name={"Bad"} />
+      <Header name={"Statistics"} />
+      <Stats name={"Good"} value={good} />
+      <Stats name={"Neutral"} value={neutral} />
+      <Stats name={"Bad"} value={bad} />
+    </div>
+  )
+}
+
+export default App
