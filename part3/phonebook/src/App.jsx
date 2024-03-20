80077ba3674b88a40581ba8ffb4ca86da261db95
t3snake <t3snake@gmail.com>
2024-03-20 09:33:51 +0530

    Finish ex 2.6

---
+++ part3/phonebook/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPersons = persons.concat({name: newName})
+    setPersons(newPersons)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={ (event) => { setNewName(event.target.value) } }/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map( (person, index) => <div key={index}>{person.name}</div> )}
+    </div>
+  )
+}
+
+export default App
