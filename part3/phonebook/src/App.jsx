78041b8cb73eb8363d5ba24d35b1cefe0144161b
t3snake <t3snake@gmail.com>
2024-03-20 09:34:47 +0530

    Change key in collection

--- part3/phonebook/src/App.jsx
+++ part3/phonebook/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map( (person, index) => <div key={index}>{person.name}</div> )}
+      {persons.map( (person) => <div key={person.name}>{person.name}</div> )}
     </div>
   )
 }
