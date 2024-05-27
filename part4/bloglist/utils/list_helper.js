68c04fb2cf6ac6fe10c8e6ea62e2e6aace90baa5
t3snake <t3snake@gmail.com>
2024-05-28 00:59:35 +0530

    Finish ex 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,6 +3,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    var likes = 0
+
+    blogs.forEach(element => {
+        if (element.likes) {
+            likes += element.likes
+        }
+    });
+
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
