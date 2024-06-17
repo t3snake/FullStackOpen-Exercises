0bfbe6cfe614bb12d8d7f1297438c1cc246bc564
t3snake <t3snake@gmail.com>
2024-06-17 15:51:22 +0530

    Finish ex 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import { useState } from "react"
 
+const Blog = ({ blog }) => {
+    const [isDetailVisible, setDetailsVisible] = useState(false)
+
+    const toggleDetails = () => {
+        setDetailsVisible(!isDetailVisible)
+    }
+
+    const blogStyle = {
+        margin: '20px',
+        border: '3px solid darkgray',
+        padding: '10px',
+        backgroundColor: 'beige'
+    }
+
+    const detailsStyle = {
+        display: isDetailVisible? '': "none",
+        textIndent: '20px'
+    }
+
+    const toggleButtonText = () => {
+        return isDetailVisible ? 'Hide': 'Show more'
+    }
+
+    const addLike = () => {
+
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} 
+            <button onClick={toggleDetails}> {toggleButtonText()} </button>
+            <div style={detailsStyle}>
+                <div>by {blog.author}</div>
+                <div>{blog.url}</div>
+                <div>
+                    {blog.likes}
+                    <button onClick={addLike}>Like</button>
+                </div>
+            </div>
+        </div>  
+      )
+      
+}
 export default Blog
