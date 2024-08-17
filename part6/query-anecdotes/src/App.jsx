e3717f40a1f63e4abf38836e4c84470ee3bbf0dd
t3snake <t3snake@gmail.com>
2024-08-17 21:51:53 +0530

    Implement anecdotes with react query for ex 6.20

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,58 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+    const handleVote = (anecdote) => {
+        console.log('vote')
+    }
+
+    const result = useQuery({
+        queryKey: ['anecdotes'],
+        queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+    })
+
+    if ( result.isLoading ) {
+        return (
+            <div>
+                Loading data...
+            </div>
+        )
+    } else if ( result.isError ) {
+        return (
+            <div>
+                Anecdote service not available due to problems in server
+            </div>
+        )
+    }
+
+    console.log(JSON.parse(JSON.stringify(result.data)))
+
+    const anecdotes = result.data
+
+    return (
+        <div>
+        <h3>Anecdote app</h3>
+        
+        <Notification />
+        <AnecdoteForm />
+        
+        {anecdotes.map(anecdote =>
+            <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
+            </div>
+        )}
+        </div>
+    )
+}
+
+export default App
