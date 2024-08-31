162ab5358411c1021d1f04ba3caefeeb1adb3189
t3snake <t3snake@gmail.com>
2024-09-01 03:21:07 +0530

    Improve routing and add notification for ex 7.3

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, Routes, Route, useMatch } from 'react-router-dom'
+import { Link, Routes, Route, useMatch, useNavigate } from 'react-router-dom'
 
 const Menu = () => {
     const padding = {
@@ -72,6 +72,8 @@ const CreateNew = (props) => {
     const [author, setAuthor] = useState('')
     const [info, setInfo] = useState('')
 
+    const navigate = useNavigate()
+
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -81,6 +83,8 @@ const CreateNew = (props) => {
             info,
             votes: 0
         })
+        navigate('/')
+
     }
 
     return (
@@ -116,28 +120,33 @@ const CreateNew = (props) => {
 }
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: 1
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: 2
-    }
-  ])
+    const [anecdotes, setAnecdotes] = useState([
+        {
+            content: 'If it hurts, do it more often',
+            author: 'Jez Humble',
+            info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+            votes: 0,
+            id: 1
+        },
+        {
+            content: 'Premature optimization is the root of all evil',
+            author: 'Donald Knuth',
+            info: 'http://wiki.c2.com/?PrematureOptimization',
+            votes: 0,
+            id: 2
+        }
+    ])
 
     const [notification, setNotification] = useState('')
 
     const addNew = (anecdote) => {
         anecdote.id = Math.round(Math.random() * 10000)
         setAnecdotes(anecdotes.concat(anecdote))
+        setNotification(`new anecdote ${anecdote.content} created!`)
+
+        setTimeout(() => {
+            setNotification('')
+        }, 5000)
     }
 
     const anecdoteById = (id) =>
@@ -163,6 +172,7 @@ const App = () => {
         <div>
             <h1>Software anecdotes</h1>
             <Menu />
+            {notification}
             <Routes>
                 <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
                 <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
