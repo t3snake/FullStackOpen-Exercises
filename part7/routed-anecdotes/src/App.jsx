b717f39b7774e45bda384bd97a604557501dafa1
t3snake <t3snake@gmail.com>
2024-09-01 16:20:13 +0530

    create custom hook to simplify forms for ex 7.4

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, Routes, Route, useMatch, useNavigate } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
     const padding = {
@@ -68,9 +69,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('content')
+    const author = useField('author')
+    const info = useField('info')
 
     const navigate = useNavigate()
 
@@ -78,9 +79,9 @@ const CreateNew = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
         navigate('/')
@@ -94,23 +95,23 @@ const CreateNew = (props) => {
             <div>
                 content
                 <input 
-                    name='content' 
-                    value={content} 
-                    onChange={(e) => setContent(e.target.value)} />
+                    name={content.type}
+                    value={content.value} 
+                    onChange={content.onChange} />
             </div>
             <div>
                 author
                 <input 
-                    name='author' 
-                    value={author} 
-                    onChange={(e) => setAuthor(e.target.value)} />
+                    name={author.type}
+                    value={author.value} 
+                    onChange={author.onChange} />
             </div>
             <div>
                 url for more info
                 <input 
-                    name='info' 
-                    value={info} 
-                    onChange={(e)=> setInfo(e.target.value)} />
+                    name={info.type}
+                    value={info.value} 
+                    onChange={info.onChange} />
             </div>
             <button>create</button>
         </form>
